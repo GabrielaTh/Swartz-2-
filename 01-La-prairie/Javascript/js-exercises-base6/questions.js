443b0c4f5a9854b07a42a77720f3e6edd3bd7235
GabrielaTh <gabrielathomasfun@gmail.com>
2018-10-25 15:46:33 +0200

    add traning -6

--- 01-La-prairie/Javascript/js-exercises-base6/questions.js
+++ 01-La-prairie/Javascript/js-exercises-base6/questions.js
@@ -1,43 +1,60 @@
-var CreationTableauLangages = function () {
+var CreationTableauLangages = function() {
+  var array = ["Html", "CSS", "Java", "PHP"];
+  return array;
+};
 
-}
+var CreationTableauNombres = function() {
+  let numTest = [0, 1, 2, 3, 4, 5];
+  return numTest;
+};
 
-var CreationTableauNombres = function () {
+var RemplacementElement = function(langages) {
+  var arreglo = langages;
+  arreglo.splice(2, 1, "Javascript");
+  return arreglo;
+};
 
-}
+var AjoutElementLangages = function(langages) {
+  langages.push("Ruby");
+  langages.push("Python");
+  return langages;
+};
 
-var RemplacementElement = function (langages) {
+var AjoutElementNombres = function(nombres) {
+  nombres.unshift(-2, -1);
+  return nombres;
+};
 
-}
+var SuppressionPremierElement = function(langages) {
+  langages.shift();
+  return langages;
+};
 
-var AjoutElementLangages = function (langages) {
+var SuppressionDernierElement = function(langages) {
+    langages.pop();
+    return langages;
+};
 
-}
+var ConversionChaineTableau = function(reseaux_sociaux_chaine) {
+    var a = reseaux_sociaux_chaine
+    var separador = a.split(",");
+    return separador;
+};
 
-var AjoutElementNombres = function (nombres) {
+var ConversionTableauChaine = function(langages) {
+    var a = langages;
+    var b = a.toString();
+    return b;
+};
 
-}
+var TriTableau = function(reseaux_sociaux) {
+   var a = reseaux_sociaux;
+   var b = a.sort();
+   return b;
+};
 
-var SuppressionPremierElement = function (langages) {
-
-}
-
-var SuppressionDernierElement = function (langages) {
-
-}
-
-var ConversionChaineTableau = function (reseaux_sociaux_chaine) {
-
-}
-
-var ConversionTableauChaine = function (langages) {
-
-}
-
-var TriTableau = function (reseaux_sociaux) {
-
-}
-
-var InversionTableau = function (reseaux_sociaux){
-
-}
+var InversionTableau = function(reseaux_sociaux) {
+var a = reseaux_sociaux;   
+var b = a.reverse();
+ return b;
+};
